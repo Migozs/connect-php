4d3e7e7974a5ee10ed29e456a6970d5577faea21
Migo <lorenzo.jphilipmiguel.t.bsit.cict@gmail.com>
2026-02-22 19:53:52 +0800

    connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,14 @@
 <?php
 
-$servername = "localhost";
+$servername ="localhost:8080";
 $username = "root";
 $password = "";
 $database = "dumplang";
 
 $conn = mysqli_connect($servername, $username, $password, $database, 3306);
+
+if ($conn->connect_error) {
+    die ("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
